e20a9b052c14e71f532eb29f6e535901fd3bcab3
suruomo <www.suruomo@hotmail.com>
2020-04-14 16:36:36 +0800

    增加model，生成新表

--- apps/message/models.py
+++ apps/message/models.py
@@ -1,3 +1,16 @@
+
 from django.db import models
 
+
 # Create your models here.
+class UserMessage(models.Model):
+    name = models.CharField(max_length=20,null=True,default="",verbose_name=u"用户名")
+    email = models.EmailField(verbose_name=u"邮箱")
+    address = models.CharField(max_length=100, verbose_name="地址")
+    message = models.CharField(max_length=500, verbose_name="留言信息")
+
+    class Meta:
+        verbose_name = u"用户留言信息"
+        verbose_name_plural=verbose_name
+        # db_table="user_message"
+        # ordering="id"
