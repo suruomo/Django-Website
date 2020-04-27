a5dd9dcf7380761476fb8a892ecbae8fa69abb65
suruomo <www.suruomo@hotmail.com>
2020-04-27 20:08:10 +0800

    用户登录逻辑1.0-基于函数

--- django_web/settings.py
+++ django_web/settings.py
@@ -30,7 +30,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+# 自定义用户登录
+AUTHENTICATION_BACKENDS=(
+    'users.views.CustomBackend',
+)
 # Application definition
 
 INSTALLED_APPS = [
--- django_web/urls.py
+++ django_web/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 import xadmin
 from django.urls import path
+
+from users.views import user_login
 # Template文件相关
 from django.views.generic import TemplateView
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
     path('', TemplateView.as_view(template_name="index.html"),name="index"),
-    path('login', TemplateView.as_view(template_name="login.html"),name="login"),
+    path('login', user_login,name="login"),
 ]
